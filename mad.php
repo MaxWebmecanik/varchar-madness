6bd8a6ef742e0384eeb1a3cc20a43a3777502553
Maximilien Mellen <mme@webmecanik.com>
2017-07-07 11:26:59 +0200

    Make column width configurable

--- mad.php
+++ mad.php
@@ -4,6 +4,8 @@ $dsn = 'mysql:host=localhost;dbname=varchar_madness';
 $user = 'varchar_madness';
 $pass = 'varchar_madness';
 
+$col_width = 255;
+
 echo "Connecting to the database...\n";
 
 $dbh = new \PDO($dsn, $user, $pass);
@@ -11,12 +13,12 @@ $dbh = new \PDO($dsn, $user, $pass);
 echo "Reseting table...\n";
 
 $dbh->exec('DROP TABLE IF EXISTS varchar_madness');
-$dbh->exec('CREATE TABLE varchar_madness (col_1 VARCHAR(255))');
+$dbh->exec("CREATE TABLE varchar_madness (col_1 VARCHAR($col_width))");
 
 echo "Creating columns...\n";
 
 for ($i = 2;; $i += 1) {
-    $row_count = $dbh->exec("ALTER TABLE varchar_madness ADD COLUMN col_$i VARCHAR(255)");
+    $row_count = $dbh->exec("ALTER TABLE varchar_madness ADD COLUMN col_$i VARCHAR($col_width)");
 
     if ($row_count === FALSE) {
         echo "Failed to create column #$i with the following error:\n";
